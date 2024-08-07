8098298eef0c882886552e529444cf48f4bbf985
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-07 12:33:44 +0530

    added greeting and cards in main.jsx file with css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Main from "./components/main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   );
 };
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,42 @@
-import './Main.css'
+import { assets } from '../../assets/assets';
+import './Main.css';
 
 const Main = () => {
     return (
-        <div>Main</div>
-    )
-}
+        <div className='main'>
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt="user icon" />
+            </div>
 
-export default Main
+            <div className="main-container">
+                <div className="greet">
+                    <p><span> Hello, Dev</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+
+                <div className="cards">
+                    <div className="card">
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compass_icon} alt="compass icon" />
+                    </div>
+                    <div className="card">
+                        <p>Briefly summarize this concept:urban planning </p>
+                        <img src={assets.bulb_icon} alt="compass icon" />
+                    </div>
+                    <div className="card">
+                        <p>Brainstorm team bonding activities for our work retreat</p>
+                        <img src={assets.message_icon} alt="compass icon" />
+                    </div>
+                    <div className="card">
+                        <p>Improve the readability of the following code</p>
+                        <img src={assets.code_icon} alt="compass icon" />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Main;
