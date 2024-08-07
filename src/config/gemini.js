7a76cc858185b4fb409373a31f2c315c1055c5c0
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-07 18:39:02 +0530

    trying to add gemini api

---
+++ src/config/gemini.js
@@ -0,0 +1,36 @@
+// const apikey = "AIzaSyDzL6k_Tn0Og0ywCbyqbo_JyT08sc_CSNc";
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+} = import("@google/generative-ai");
+
+// const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI("AIzaSyDzL6k_Tn0Og0ywCbyqbo_JyT08sc_CSNc");
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-1.0-pro",
+});
+
+const generationConfig = {
+    temperature: 0.9,
+    topP: 1,
+    maxOutputTokens: 2048,
+    responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+    const chatSession = model.startChat({
+        generationConfig,
+        // safetySettings: Adjust safety settings
+        // See https://ai.google.dev/gemini-api/docs/safety-settings
+        history: [
+        ],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    console.log(result.response.text());
+}
+
+export default run;
---
+++ src/context/Context.jsx
@@ -0,0 +1,24 @@
+import { createContext } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const onSent = async (prompt) => {
+        await run(prompt);
+    };
+
+    // onSent("What is react js ?");
+
+    const contextValue = {
+
+    };
+    return (
+        <ContextProvider value={contextValue}>
+            {props.children}
+        </ContextProvider>
+    );
+};
+
+export default ContextProvider;
